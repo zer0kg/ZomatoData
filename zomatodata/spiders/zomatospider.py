cc033f8db93411fe0ef806f146d4ce4d8907d9f8
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-14 19:06:08 +0530

    Add URLs for each city

--- zomatodata/spiders/zomatospider.py
+++ zomatodata/spiders/zomatospider.py
@@ -1,10 +1,22 @@
 from scrapy.spider import Spider
 from scrapy.selector import Selector
 
+from zomatodata.items import Restaurant
+
+s = Restaurant()
 
 
-
-
-
-
+class ZomatoSpider(Spider):
+    name = 'zomatospider'
+    allowed_domains = ['zomato.com']
+    start_urls = [
+        'https://www.zomato.com/chennai/restaurants?page=1',
+        'https://www.zomato.com/bangalore/restaurants?page=1',
+        'https://www.zomato.com/mysore/restaurants?page=1',
+        'https://www.zomato.com/pune/restaurants?page=1',
+        'https://www.zomato.com/mumbai/restaurants?page=1',
+        'https://www.zomato.com/hyderabad/restaurants?page=1',
+        'https://www.zomato.com/kolkata/restaurants?page=1',
+        'https://www.zomato.com/ncr/restaurants?page=1',
+    ]
 
