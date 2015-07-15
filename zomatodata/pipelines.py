0f851b1513df54702ddb12f5955f159f2200ad29
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-15 23:01:35 +0530

    Add item pipeline for address and postal code

--- zomatodata/pipelines.py
+++ zomatodata/pipelines.py
@@ -5,15 +5,31 @@ class CostPipeline(object):
         cost = item['cost']
         try:
             item['cost'] = int(re.sub('[^0-9]+', '', cost))
-            print "PART REACHED"
         except ValueError:
             item['cost'] = 'NA'
         return item
 
 class RatingPipeline(object):
     def process_item(self, item, spider):
+        print "RATING =", item['rating']
         try:
-            item['rating'] = int(item['rating'].strip())
+            item['rating'] = float(item['rating'].strip())
         except ValueError:
-            item['rating'] = 'NA'
+            item['rating'] = float('nan')
         return item
+
+class PostalCodePipeline(object):
+    def process_item(self, item, spider):
+        if len(item['r_postcode']) == 0:
+            item['r_postcode'] = 'NA'
+        else:
+            item['r_postcode'] = int(item['r_postcode'][0])
+
+        return item
+
+class AddressPipeline(object):
+    def process_item(self, item, spider):
+        add = ''.join(map(lambda x: x.strip(), item['r_address']))
+        item['r_address'] = add
+        return item
+
