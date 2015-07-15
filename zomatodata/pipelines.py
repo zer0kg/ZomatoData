bab76cc473623a9581b414c229471d03a59643c2
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-15 22:08:07 +0530

    Add item pipeline for rating number

--- zomatodata/pipelines.py
+++ zomatodata/pipelines.py
@@ -5,6 +5,15 @@ class CostPipeline(object):
         cost = item['cost']
         try:
             item['cost'] = int(re.sub('[^0-9]+', '', cost))
+            print "PART REACHED"
         except ValueError:
             item['cost'] = 'NA'
+        return item
+
+class RatingPipeline(object):
+    def process_item(self, item, spider):
+        try:
+            item['rating'] = int(item['rating'].strip())
+        except ValueError:
+            item['rating'] = 'NA'
         return item
