da6cd9b4d8cd203c8a7e42a7964a297b08d8db7d
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-15 20:05:00 +0530

    Add item pipelines
    Added the CostPipeline to the item pipelines dict with value 1.

--- zomatodata/settings.py
+++ zomatodata/settings.py
@@ -2,3 +2,6 @@ SPIDER_MODULES = ['zomatodata.spiders']
 NEWSPIDER_MODULE = 'zomatodata.spiders'
 DEFAULT_ITEM_CLASS = 'zomatodata.items.Restaurant'
 
+ITEM_PIPELINES = {
+    'zomatospider.pipelines.CostPipeline': 1
+}
