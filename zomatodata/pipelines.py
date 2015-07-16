dd73fbc4e968e4a81aeca67b9ae943a045125b30
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-16 09:47:16 +0530

    Add pipelines for rest info
    Converting strings into proper ints and floats and adding conditions for NA values.

--- zomatodata/pipelines.py
+++ zomatodata/pipelines.py
@@ -11,11 +11,12 @@ class CostPipeline(object):
 
 class RatingPipeline(object):
     def process_item(self, item, spider):
-        print "RATING =", item['rating']
+        # print "RATING =", item['rating']
         try:
+            item['rating'] = item['rating'][0]
             item['rating'] = float(item['rating'].strip())
-        except ValueError:
-            item['rating'] = float('nan')
+        except ValueError or IndexError:
+            item['rating'] = 'NA'
         return item
 
 class PostalCodePipeline(object):
@@ -33,3 +34,41 @@ class AddressPipeline(object):
         item['r_address'] = add
         return item
 
+class OtherInfoPipeline(object):
+    def process_item(self, item, spider):
+        try:
+            item['bookmarks'] = int(item['bookmarks'][0])
+        except ValueError or IndexError:
+            item['bookmarks'] = 'NA'
+
+        try:
+            item['checkins'] = int(item['checkins'][0])
+        except ValueError or IndexError:
+            item['checkins'] = 'NA'
+
+        try:
+            item['photos'] = int(item['photos'][0])
+        except ValueError or IndexError:
+            item['photos'] = 'NA'
+
+        try:
+            item['reviews'] = int(item['reviews'][0])
+        except ValueError or IndexError:
+            item['reviews'] = 'NA'
+
+        try:
+            item['rating_votes'] = int(item['rating_votes'][0])
+        except ValueError or IndexError:
+            item['rating_votes'] = 'NA'
+        return item
+
+class LocationPipeline(object):
+    def process_item(self, item, spider):
+        l = len(item['r_latitude'])
+        if l == 0:
+            item['r_latitude'] = 'NA'
+            item['r_longitude'] = 'NA'
+        elif l != 0:
+            item['r_latitude'], item['r_longitude'] = map(float, item['r_latitude'])
+
+        return item
