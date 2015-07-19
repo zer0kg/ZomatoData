ed3baa3d9317b21088f809156e1e739ba85799df
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-19 19:47:04 +0530

    Replace w with wb to prevent extra line in CSV file

--- zomatodata/pipelines.py
+++ zomatodata/pipelines.py
@@ -1,16 +1,10 @@
-import re
 import csv
 from items import Restaurant
-import unicodedata
-
-
-def unicode_convert(s):
-    return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
 
 class CSVPipeline(object):
     def __init__(self):
         names = Restaurant().fields
-        self.csv_file = open('res.csv', 'w')
+        self.csv_file = open('res.csv', 'wb')
         self.file = csv.DictWriter(self.csv_file, fieldnames=names)
         self.file.writeheader()
 
