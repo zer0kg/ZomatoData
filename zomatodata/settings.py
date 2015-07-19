0004b1c6f1c2c9d4c163ff73537327c8ed7d79d4
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-19 19:05:19 +0530

    Remove pipelines from settings
    Pipelines from pipelines.py have been removed.

--- zomatodata/settings.py
+++ zomatodata/settings.py
@@ -3,11 +3,5 @@ NEWSPIDER_MODULE = 'zomatodata.spiders'
 DEFAULT_ITEM_CLASS = 'zomatodata.items.Restaurant'
 
 ITEM_PIPELINES = {
-    'zomatodata.pipelines.CostPipeline': 300,
-    'zomatodata.pipelines.RatingPipeline': 400,
-    'zomatodata.pipelines.PostalCodePipeline': 500,
-    'zomatodata.pipelines.AddressPipeline': 600,
-    'zomatodata.pipelines.OtherInfoPipeline': 700,
-    'zomatodata.pipelines.LocationPipeline': 800,
-    'zomatodata.pipelines.CSVPipeline': 1000
+    'zomatodata.pipelines.CSVPipeline': 1000,
 }
