e0911f88df73b735ec0d7ef3ecfceeecb5844942
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-19 19:48:43 +0530

    Multiple small changes
    1. Added () in front of Restaurant
    2. changed callback to parse_rest2
    3. added yield statement to parse_rest2 method

--- zomatodata/spiders/zomatospider.py
+++ zomatodata/spiders/zomatospider.py
@@ -25,7 +25,7 @@ class ZomatoSpider(Spider):
         global COUNT
         for rest in response.css('a.result-title'):
             url = rest.xpath('@href').extract()[0]
-            yield scrapy.Request(url, callback=self.parse_rest)
+            yield scrapy.Request(url, callback=self.parse_rest2)
 
         next_link = response.css('li.current + li.active a').xpath('@href').extract()
         print "NEXT LINK:", next_link
@@ -60,13 +60,13 @@ class ZomatoSpider(Spider):
         yield rest
 
     def parse_rest2(self, response):
-        rest = RestItemLoader(item=Restaurant, response=response)
+        rest = RestItemLoader(item=Restaurant(), response=response)
 
         rest.add_xpath('r_name', '//h1/a/span/text()')
         rest.add_xpath('r_id', '//*/@data-res-id')
         rest.add_css('r_type', 'div.res-info-estabs > a::text')
         rest.add_value('link', response.url)
-        rest.add_value('city',  findall('\\.com\/([a-z]+)\/', rest.item['link']))
+        rest.add_value('city',  findall('\\.com\/([a-z]+)\/', rest.get_value('link')))
         rest.add_css('cost', 'span[itemprop="priceRange"]::text')
         rest.add_css('area', 'span[itemprop="addressLocality"]::text')
         rest.add_css('rating', 'div[itemprop="ratingValue"]::text')
@@ -81,7 +81,7 @@ class ZomatoSpider(Spider):
         rest.add_value('r_latitude', value=response.selector.re("\|([\d.]+),[\d.]+\|"))
         rest.add_value('r_longitude', value=response.selector.re("\|[\d.]+,([\d.]+)\|"))
 
-
+        yield rest.load_item()
 
 
 
