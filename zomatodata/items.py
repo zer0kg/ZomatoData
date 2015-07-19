377ae069371ded3e9896db8b8c38d3ed76a915c3
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-19 18:12:09 +0530

    Remove postal code Field
    Add a new ItemLoader class for processing inputs

--- zomatodata/items.py
+++ zomatodata/items.py
@@ -1,4 +1,8 @@
 from scrapy.item import Item, Field
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose
+import unicodedata
+import re
 
 # TODO: Rewrite the below class to include Item Loaders
 
@@ -7,7 +11,6 @@ class Restaurant(Item):
     r_name = Field()
     r_id = Field()
     r_type = Field()
-    r_postcode = Field()
     r_address = Field()
     city = Field()
     link = Field()
@@ -23,3 +26,19 @@ class Restaurant(Item):
     collections = Field()
     r_latitude = Field()
     r_longitude = Field()
+
+def unicode_convert(s):
+    return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
+
+class RestItem(ItemLoader):
+    default_input_processor = MapCompose(unicode_convert)
+    default_output_processor = TakeFirst()
+
+    r_id_in = MapCompose(int)
+
+    cost_in = MapCompose(lambda x: re.sub('[^0-9]+', '', x), int)
+
+    area_in = MapCompose()
+
+
+
