0d5928d3ee4211b0e5f0f385a0ad0d03d57659f8
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-20 18:16:55 +0530

    Remove parse_rest2 function and rename parse_rest2

--- zomatodata/spiders/zomatospider.py
+++ zomatodata/spiders/zomatospider.py
@@ -23,41 +23,14 @@ class ZomatoSpider(Spider):
         global COUNT
         for rest in response.css('a.result-title'):
             url = rest.xpath('@href').extract()[0]
-            yield scrapy.Request(url, callback=self.parse_rest2)
+            yield scrapy.Request(url, callback=self.parse_rest)
 
         next_link = response.css('li.current + li.active a').xpath('@href').extract()
         print "NEXT LINK:", next_link
         if next_link:
             yield scrapy.Request(response.urljoin(next_link[0]), callback=self.parse)
 
-
     def parse_rest(self, response):
-        rest = Restaurant()
-
-        rest['r_name'] = response.xpath('//h1/a/span/text()').extract()[0]
-        self.logger.info('Scraper is currently parsing %s' % rest['r_name'])
-        rest['r_id'] = response.xpath('//*/@data-res-id').extract()[0]
-        rest['r_type'] = response.css('div.res-info-estabs > a::text').extract()[0]
-        rest['link'] = response.url
-        rest['city'] = findall('\\.com\/([a-z]+)\/', rest['link'])[0]
-        rest['cost'] = response.css('span[itemprop="priceRange"]::text').extract()
-        rest['area'] = response.css('span[itemprop="addressLocality"]::text').extract()
-        rest['rating'] = response.css('div[itemprop="ratingValue"]::text').extract()
-        rest['rating_votes'] = response.css("span[itemprop='ratingCount']::text").extract()
-        rest['reviews'] = response.css("div.res-main-stats-num::text").extract()
-        rest['photos'] = response.css("div#ph_count::text").extract()
-        rest['bookmarks'] = response.css("div#wtt_count::text").extract()
-        rest['checkins'] = response.css("div#bt_count::text").extract()
-        rest['cuisines'] = response.css("a[itemprop='servesCuisine']::text").extract()
-        rest['collections'] = response.css("span.res-page-collection-text > a::text").extract()
-        rest['r_address'] = response.css("div.res-main-address-text::text").extract()
-        rest['r_latitude'] = response.selector.re("\|([\d.]+),[\d.]+\|")
-        rest['r_longitude'] = response.selector.re("\|[\d.]+,([\d.]+)\|")
-
-
-        yield rest
-
-    def parse_rest2(self, response):
         rest = RestItemLoader(item=Restaurant(), response=response)
 
         rest.add_xpath('r_name', '//h1/a/span/text()')
