cf41bdacfea6d66e7842217f5f5eca0583ee6b85
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-25 07:51:46 +0530

    Uncomment start_urls

--- zomatodata/spiders/zomatospider.py
+++ zomatodata/spiders/zomatospider.py
@@ -8,14 +8,14 @@ class ZomatoSpider(Spider):
     name = 'zomatospider'
     allowed_domains = ['zomato.com']
     start_urls = [
-        'https://www.zomato.com/chennai/restaurants?page=110',
-        # 'https://www.zomato.com/bangalore/restaurants?page=1',
-        # 'https://www.zomato.com/mysore/restaurants?page=1',
-        # 'https://www.zomato.com/pune/restaurants?page=1',
-        # 'https://www.zomato.com/mumbai/restaurants?page=1',
-        # 'https://www.zomato.com/hyderabad/restaurants?page=1',
-        # 'https://www.zomato.com/kolkata/restaurants?page=1',
-        # 'https://www.zomato.com/ncr/restaurants?page=1',
+        'https://www.zomato.com/chennai/restaurants?page=1',
+        'https://www.zomato.com/bangalore/restaurants?page=1',
+        'https://www.zomato.com/mysore/restaurants?page=1',
+        'https://www.zomato.com/pune/restaurants?page=1',
+        'https://www.zomato.com/mumbai/restaurants?page=1',
+        'https://www.zomato.com/hyderabad/restaurants?page=1',
+        'https://www.zomato.com/kolkata/restaurants?page=1',
+        'https://www.zomato.com/ncr/restaurants?page=1',
     ]
 
 
