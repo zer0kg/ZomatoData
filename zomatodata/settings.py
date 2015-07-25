680304510af7bc47b2cadbf076e1e5a1554b203d
Kaushik Ganesh <kaushik95@gmail.com>
2015-07-25 07:41:20 +0530

    Temporarily remove Item Pipelines so that spider can deployed to ScrapingHub

--- zomatodata/settings.py
+++ zomatodata/settings.py
@@ -2,6 +2,3 @@ SPIDER_MODULES = ['zomatodata.spiders']
 NEWSPIDER_MODULE = 'zomatodata.spiders'
 DEFAULT_ITEM_CLASS = 'zomatodata.items.Restaurant'
 
-ITEM_PIPELINES = {
-    'zomatodata.pipelines.CSVPipeline': 1000,
-}
